24b736584a34ff114b92a5ec6ba3de4ea97ffc35
AlinaShklyaruk <i.charge92@gmail.com>
2022-10-18 23:36:14 +0300

    first task is ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const bodyEl = document.querySelector('body');
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+let intervalId = null;
+
+function onStartBtnClick() {
+    startBtn.disabled = true;
+    intervalId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    console.log(intervalId);
+}
+
+function onStopBtnClick() {
+    //if (onStartBtnClick) {
+   { startBtn.disabled = false;
+    clearInterval(intervalId);
+    }
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
