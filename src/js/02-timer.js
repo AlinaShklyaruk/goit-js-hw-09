d7db210febc7201b3a77cae0d9933e279e23e453
AlinaShklyaruk <i.charge92@gmail.com>
2022-10-19 23:40:24 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,2 +1,21 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
+const inputEl = document.querySelector('input#datetime-picker');
+const startBtn = document.querySelector('button[data-start]');
+const timerEl = document.querySelector('.timer');
+const fieldsEl = document.querySelectorAll('.field');
+//console.log(timerEl);
+
+timerEl.style.display = 'flex';
+timerEl.style.marginRight = '-10px';
+fieldsEl.forEach((field => {
+    field.style.display = 'flex';
+    field.style.alignItems = 'center';
+    field.style.flexDirection = 'column';
+    field.style.marginRight = '10px';
+}));
+
+
+
+const fp = flatpickr(inputEl, {});
