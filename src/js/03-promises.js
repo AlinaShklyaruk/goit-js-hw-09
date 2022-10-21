1644d5f647d8bd2c7f309f7696ab00ec2563c013
AlinaShklyaruk <i.charge92@gmail.com>
2022-10-21 19:15:40 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,19 @@
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.5.min.css";
+
+const formEl = document.querySelector('.form');
+const delayEl = document.querySelector('input[name="delay"');
+const stepEl = document.querySelector('input[name="step"]');
+const amountEl = document.querySelector('input[name="amount"]');
+const submitBtn = document.querySelector('button[type="submit"]');
+
+formEl.addEventListener('submit', onSubmitBtnClick);
+
+function onSubmitBtnClick(event) {
+  event.preventDefault();
+  
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
